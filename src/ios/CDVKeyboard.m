36e8f9058bbc473ed51f5cd414506feba1257f8b
James Sharp <james@ortootech.com>
2016-02-18 23:50:39 +0000

    Allow iPad split screen

--- src/ios/CDVKeyboard.m
+++ src/ios/CDVKeyboard.m
@@ -160,7 +160,7 @@ static IMP WKOriginalImp;
 
     self.webView.scrollView.scrollEnabled = YES;
 
-    CGRect screen = [[UIScreen mainScreen] bounds];
+    CGRect screen = self.viewController.view.window.frame;
     CGRect statusBar = [[UIApplication sharedApplication] statusBarFrame];
     CGRect keyboard = ((NSValue*)notif.userInfo[@"UIKeyboardFrameEndUserInfoKey"]).CGRectValue;
 
